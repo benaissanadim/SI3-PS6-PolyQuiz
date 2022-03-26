ee70ccae7937e9da6e7f94c4f692a73cb01ed351
Hamza ZOUBAIR <91201878+Hamza7306-hash@users.noreply.github.com>
2022-03-26 21:50:04 +0100

    Edit question_model back-end #2

--- back-end/app/models/question.model.js
+++ back-end/app/models/question.model.js
@@ -4,5 +4,7 @@ const BaseModel = require('../utils/base-model.js')
 module.exports = new BaseModel('Question', {
   label: Joi.string().required(),
   quizId: Joi.number(),
+  image : Joi.string(),
+  indice : Joi.string(),
   answers: Joi.array()
 })
