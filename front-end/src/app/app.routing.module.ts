f7f3a207a6ddcc080cf08699239f4a8ec09a02ab
BEN AISSA Nadim <80357350+benaissanadim@users.noreply.github.com>
2022-03-27 19:21:30 +0200

    add routes
    #4

--- front-end/src/app/app.routing.module.ts
+++ front-end/src/app/app.routing.module.ts
@@ -3,12 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { QuizListComponent } from './quizzes/quiz-list/quiz-list.component';
 import { EditQuizComponent } from './quizzes/edit-quiz/edit-quiz.component';
 import { QuizFormComponent } from './quizzes/quiz-form/quiz-form.component';
+import { QuestionFormComponent } from './questions/question-form/question-form.component';
 
 const routes: Routes = [
     {path: 'quiz-list', component: QuizListComponent},
     {path: 'edit-quiz/:id', component: EditQuizComponent},
     { path: '', redirectTo: '/quiz-list', pathMatch: 'full' },
-   {path: 'quiz-add', component : QuizFormComponent},
+    {path: 'quiz-add', component : QuizFormComponent},
+    {path: 'add-question/:id',component:  QuestionFormComponent }
+   
 ];
 
 @NgModule({
