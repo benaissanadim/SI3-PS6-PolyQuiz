a4d3daf579ae9e3f0e9b00adae4dc797f59a6ff9
BEN AISSA Nadim <80357350+benaissanadim@users.noreply.github.com>
2022-03-28 22:53:27 +0200

    add final recap for quiz
    #7

---
+++ front-end/src/app/play/recap-quiz/recap-quiz.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Answer, Question } from 'src/models/question.model';
+import { Quiz } from 'src/models/quiz.model';
+import { QuizService } from 'src/services/quiz.service';
+
+@Component({
+  selector: 'app-recap-quiz',
+  templateUrl: './recap-quiz.component.html',
+  styleUrls: ['./recap-quiz.component.scss']
+})
+export class RecapQuizComponent implements OnInit {
+  public question: Question;
+  public answer: Answer;
+  public quiz: Quiz;
+  public id
+  
+constructor(private route: ActivatedRoute,private quizService: QuizService,private router: Router) {
+    this.quizService.quizSelected$.subscribe((quiz) => (this.quiz = quiz));
+  }
+
+  ngOnInit(): void {
+    this.id = this.route.snapshot.paramMap.get('id');
+    this.quizService.setSelectedQuiz(this.id);
+  }
+
+  getCorrectAnswer(index) {
+    for (let i = 0; i < 4; i++) {
+      if (this.quiz.questions[index].answers[i].isCorrect) {
+        return this.quiz.questions[index].answers[i];
+      }
+    }
+  }
+
+  replay(){
+    window.location.reload();
+  }
+  
+  exit(){
+    console.log("test")
+    this.router.navigate(['/quiz-list/']);
+  }
+
+}
