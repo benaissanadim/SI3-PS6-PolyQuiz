72025823cf40f6cf5a54354c35f494e8c74e8480
SAISSIOMAR <omar.saissi@etu.univ-cotedazur.fr>
2022-03-29 11:36:58 +0200

    modification dans edit-quiz.html et résoudre un bug lors du chargement de la page modifier le quiz #12

--- front-end/src/app/quizzes/edit-quiz/edit-quiz.component.ts
+++ front-end/src/app/quizzes/edit-quiz/edit-quiz.component.ts
@@ -3,6 +3,8 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { Quiz } from '../../../models/quiz.model';
 import { QuizService } from '../../../services/quiz.service';
+import { Router } from '@angular/router';
+import { Question } from 'src/models/question.model';
 
 @Component({
   selector: 'app-edit-quiz',
@@ -13,21 +15,31 @@ export class EditQuizComponent implements OnInit {
 
   public quiz: Quiz;
   public quizForm: FormGroup;
+  quizUrl: string;
+  id: string;
 
 
-  constructor(private route: ActivatedRoute,public formBuilder: FormBuilder, private quizService: QuizService) { 
-    this.quizService.quizSelected$.subscribe((quiz) => this.quiz = quiz);
-    
-    this.quizForm = this.formBuilder.group({
-      name: [''],
-      theme: [''],
-      image:['']
+  constructor(public router:Router ,private route: ActivatedRoute,public formBuilder: FormBuilder, private quizService: QuizService) {
+    this.quizService.quizSelected$.subscribe((quiz) =>{
+      this.quiz = quiz
+     console.log("into subscribe",quiz);
+      this.quizForm = this.formBuilder.group({
+        name: [quiz.name],
+        theme: [quiz.theme],
+        image:[quiz.image]
+      });
     });
+    console.log("after subscribe", this.quiz)
+
+
   }
 
   ngOnInit() {
-    const id = this.route.snapshot.paramMap.get('id');
-    this.quizService.setSelectedQuiz(id);
+    this.id = this.route.snapshot.paramMap.get('id');
+    this.quizService.setSelectedQuiz(this.id);
   }
-  
+  editquiz(){
+    this.quizService.updateQuiz(this.quiz)
+    this.router.navigate(['/quiz-list']);
+  }
 }
